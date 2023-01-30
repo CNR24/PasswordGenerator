ef4b76bdfa32d1b6837df79d37984bde1dd02d4c
Caner <ic20b038technikum-wien.at>
2023-01-30 18:55:24 +0100

    new branch and class created, class updated

---
+++ src/main/java/at/fhtw/passwordgenerator/generator/Generator.java
@@ -0,0 +1,16 @@
+package at.fhtw.passwordgenerator.generator;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class Generator {
+
+    GetMapping("api/passwordgenarator")
+        public <laenge> String passwordgenerator(laenge){
+
+        }
+
+
+

d3e69371c75169bfc35457118b7b313f1e6c3eb1
Caner <ic20b038technikum-wien.at>
2023-01-30 18:01:13 +0100

    Initial commit

---
+++ src/test/java/at/fhtw/passwordgenerator/PasswordGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.passwordgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
